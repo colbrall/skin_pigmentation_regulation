1143c778ee6cbc54dd1b29407b14dbb76df817f2
Laura Colbran <colbranl@node153.hpc.local>
2020-10-27 13:56:35 -0400

    Zhang melanocyte processing

---
+++ bin/train_predixcan/build_gene_models/post_process_zhang.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# updated to run on Slurm with Python 3 by Laura Colbran 12/2018
+# first don't forget to run:
+# ml GCC/5.4.0-2.26  OpenMPI/1.10.3 R/3.3.3 R-bundle-Bioconductor
+
+import subprocess
+
+from model_parameters_1240k_Zhang import *
+
+for i, study in enumerate(STUDY_NAMES):
+    subprocess.call(
+        ['../data_processing/make_all_results_zhang.sh',study,ALL_RESULTS_FILES[i],ALPHA,SNPSET])
+    subprocess.call(
+        ['../data_processing/make_all_betas_zhang.sh',study,ALL_BETAS_FILES[i],ALPHA,SNPSET])
+    subprocess.call(['../data_processing/make_all_logs_zhang.sh',study,ALL_LOGS_FILES[i]])
+    subprocess.call(
+        ['../data_processing/make_all_covariances_zhang.sh',study,ALL_COVARIANCES_FILES[i],ALPHA,SNPSET])
+
+for i, study, in enumerate(STUDY_NAMES):
+    cmd = '../data_processing/make_sqlite_db.py --output {0} --betas {1} --results {2} --construction {3} --meta {4}'.format(
+        DB_FILES[i], ALL_BETAS_FILES[i], ALL_RESULTS_FILES[i], ALL_LOGS_FILES[i], ALL_META_DATA_FILES[i])
+    subprocess.call(cmd, shell=True)
+
+for i, study in enumerate(STUDY_NAMES):
+    print("Filtering " + study + " on significance.")
+    subprocess.call(['Rscript', '../data_processing/filter_on_significance.R', DB_FILES[i],
+        INTER_DIR + GENE_ANNOT_INTER2, FILTERED_DB_FILES[i]])
