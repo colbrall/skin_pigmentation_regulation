253d17ceec250030265edab7650250df8ded61af
Laura Colbran <colbranl@node153.hpc.local>
2020-10-27 13:57:26 -0400

    training pipeline for bsub

---
+++ bin/train_predixcan/build_gene_models/train_models_bsub.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# @author Laura Colbran, 10/2020
+# adapted from train_models.py to run on bsub with Python 3
+
+import subprocess
+import time
+
+from model_parameters_1240k_Zhang import *
+
+CMD = 'bsub -W 24:00 -M 22000 -e models_{0}_%J.error -o models_{0}_%J.out -J models_{0} ' + \
+    '"./train_predixcan.sh {0} {1} {2} {3} {4} {5} ' + \
+    '{6} {7} {8} {9} {10} {11}"'
+
+geno_prefixes = list(GENOTYPE_INTER_PREFIX)
+gene_annot = INTER_DIR + GENE_ANNOT_INTER2
+for i, study in enumerate(STUDY_NAMES):
+    for j in range(1,22):
+        expression_RDS = INTER_DIR + EXPRESSION_INTER_DIR + study + ".RDS"
+        geno = INTER_DIR + GENOTYPE_INTER_DIR + geno_prefixes[0] + '.chr'
+        snp_annot = INTER_DIR + SNP_ANN_INTER_DIR + SNP_ANN_INTER_PREFIX2
+        cmd = CMD.format(j,study,expression_RDS,geno,gene_annot,snp_annot,
+            N_K_FOLDS,ALPHA,MODEL_BY_CHR_DIR,SNPSET,WINDOW,HOME_DIR)
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+        time.sleep(2)
---
+++ bin/train_predixcan/build_gene_models/train_predixcan.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#make sure R >3.3 is available
+source activate R_peer
+
+chr=$1
+study=$2
+expr_RDS=$3
+geno=$4
+gene_annot=$5
+snp_annot=$6
+n_k_folds=$7
+alpha=$8
+out_dir=$9
+snpset=${10}
+window=${11}
+home_dir=${12}
+
+gen=$geno$chr'.txt'
+snps=$snp_annot$chr'.RDS'
+
+Rscript ./create_model.R $study $expr_RDS $gen $gene_annot $snps \
+    $n_k_folds $alpha $out_dir $chr $snpset $window
