4454a8deccbec326448399b2649a3cc5df3f9ba4
Laura Colbran <colbranl@sciget.pmacs.upenn.edu>
2021-07-05 11:27:51 -0400

    removed extraneous plotting things from another analysis

--- bin/qqplot.jl
+++ bin/qqplot.jl
@@ -9,33 +9,18 @@ using RCall, MultipleTesting, HypothesisTests
 using Plots
 
 DEG_F = 1
-# DEG_F = 2
-CUTOFF = 0.003460225947011826 #to add line to plot
-GENES = []
 
-# GENES = ["ENSG00000115850","ENSG00000149485","ENSG00000134824","ENSG00000214435",
-# "ENSG00000211448","ENSG00000131871","ENSG00000233276","ENSG00000211445",
-# "ENSG00000149187","ENSG00000234494","ENSG00000109618","ENSG00000010704",
-# "ENSG00000103671","ENSG00000165125","ENSG00000014824","ENSG00000138821",
-# "ENSG00000197208","ENSG00000197375","ENSG00000164463","ENSG00000196616"] #all rees diet genes
+GENES = ["ENSG00000161091","ENSG00000167986","ENSG00000104044",
+"ENSG00000128731","ENSG00000188467","ENSG00000077498","ENSG00000107165",
+"ENSG00000164175","ENSG00000258839","ENSG00000101440","ENSG00000115138",
+"ENSG00000080166","ENSG00000049130","ENSG00000157404","ENSG00000137265",
+"ENSG00000176797","ENSG00000185664","ENSG00000162341","ENSG00000187098",
+"ENSG00000197535","ENSG00000047579","ENSG00000069974","ENSG00000088812",
+"ENSG00000143669","ENSG00000115648","ENSG00000166189","ENSG00000134160",
+"ENSG00000151694","ENSG00000173157","ENSG00000146648","ENSG00000145244",
+"ENSG00000112038","ENSG00000157168","ENSG00000173068","ENSG00000040531",
+"ENSG00000136160","ENSG00000124205"] #skin pigmentation genes
 
-# GENES = ["ENSG00000161091","ENSG00000167986","ENSG00000104044",
-# "ENSG00000128731","ENSG00000188467","ENSG00000077498","ENSG00000107165",
-# "ENSG00000164175","ENSG00000258839","ENSG00000101440","ENSG00000115138",
-# "ENSG00000080166","ENSG00000049130","ENSG00000157404","ENSG00000137265",
-# "ENSG00000176797","ENSG00000185664","ENSG00000162341","ENSG00000187098",
-# "ENSG00000197535","ENSG00000047579","ENSG00000069974","ENSG00000088812",
-# "ENSG00000143669","ENSG00000115648","ENSG00000166189","ENSG00000134160",
-# "ENSG00000151694","ENSG00000173157","ENSG00000146648","ENSG00000145244",
-# "ENSG00000112038","ENSG00000157168","ENSG00000173068","ENSG00000040531",
-# "ENSG00000136160","ENSG00000124205"] #skin pigmentation genes
-
-GENES = ["ENSG00000149485"] #FADS1
-GENES_5 = ["ENSG00000139572"] #GPR84
-GENES_4 = ["ENSG00000197375"] #SLC22A5
-GENES_2 = ["ENSG00000116678"] #LEPR
-GENES_3 = ["ENSG00000233276"] #GPX1
-GENES_1 = ["ENSG00000134824"] #FADS2
 
 # parses command-line arguments
 function parseCommandLine()
@@ -104,21 +89,10 @@ function qqPlot(path::String,col::Int64,out::String,gc::Bool,multi::Bool)
 	println(pvalue(ApproximateTwoSampleKSTest(results[[x in GENES for x in results[:,1]],:corr_pval],results[[!(x in GENES) for x in results[:,1]],:corr_pval])))
 
     qq = Plots.plot(x,x,color = :grey,xlabel="-log10(Expected P)",ylabel = "-log10(observed P)",margin=7Plots.mm,grid=false) #,lims=(0,maximum(-log10.(results[!,:corr_pval])))
-	qq = Plots.plot!(x,-log10.([CUTOFF,CUTOFF]),color=:red,alpha=0.5)
 	qq = Plots.scatter!(results[[!in(x,GENES) for x in results[:,1]],:exp],-log10.(results[[!in(x,GENES) for x in results[:,1]],:corr_pval]),
             legend = false,color = :black, alpha = 0.5,markersize=3)
     qq = Plots.scatter!(results[[x in GENES for x in results[:,1]],:exp],-log10.(results[[x in GENES for x in results[:,1]],:corr_pval]),
             legend = false,color = :dodgerblue, alpha = 1,markershape=:vline,markersize=9)
-    qq = Plots.scatter!(results[[x in GENES_1 for x in results[:,1]],:exp],-log10.(results[[x in GENES_1 for x in results[:,1]],:corr_pval]),
-            legend = false,color = :orange, alpha = 1,markershape=:vline,markersize=9)
-    qq = Plots.scatter!(results[[x in GENES_2 for x in results[:,1]],:exp],-log10.(results[[x in GENES_2 for x in results[:,1]],:corr_pval]),
-            legend = false,color = :firebrick2, alpha = 1,markershape=:vline,markersize=9)
-    qq = Plots.scatter!(results[[x in GENES_3 for x in results[:,1]],:exp],-log10.(results[[x in GENES_3 for x in results[:,1]],:corr_pval]),
-            legend = false,color = :darkmagenta, alpha = 1,markershape=:vline,markersize=9)
-    qq = Plots.scatter!(results[[x in GENES_4 for x in results[:,1]],:exp],-log10.(results[[x in GENES_4 for x in results[:,1]],:corr_pval]),
-            legend = false,color = :lightgreen, alpha = 1,markershape=:vline,markersize=9)
-    qq = Plots.scatter!(results[[x in GENES_5 for x in results[:,1]],:exp],-log10.(results[[x in GENES_5 for x in results[:,1]],:corr_pval]),
-            legend = false,color = :pink, alpha = 1,markershape=:vline,markersize=9)
     Plots.savefig("$(out)qqplot.pdf")
 	CSV.write("$(out)corrected.txt",results;delim="\t")
 end
